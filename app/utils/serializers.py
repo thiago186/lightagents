743d17f946aa940a0fdc90bde59fe13da2321e26
Thiago Amorim <thiago.amorim@suno.com.br>
2024-10-22 21:55:52 -0300

    :art: improved linting for serializers

--- app/utils/serializers.py
+++ app/utils/serializers.py
@@ -1,17 +1,13 @@
-from typing import Any, Dict, List
-
+from typing import Any, List
 
 from app.schemas.messages_schemas import (
     Message,
-    MessageRole,
-    MessageType,
-    ToolUseMessage
 )
 
 
 def base_serializer(
     messages: List[Message], **kwargs: Any
-    ) -> List[dict[str, str]]:
+) -> List[dict[str, str]]:
     """Serialize only messages role and content."""
     if not isinstance(messages, list):
         raise ValueError("The messages must be a list.")
@@ -19,24 +15,22 @@ def base_serializer(
     serialized_messages = []
     for message in messages:
         serialized_messages.append(
-                {
-                    "content": message.content,
-                    "role": message.role.value
-                }
-            )
+            {"content": message.content, "role": message.role.value}
+        )
     return serialized_messages
+
 
 def role_mapping_serializer(
     messages: List[Message], **kwargs: Any
-    ) -> List[dict[str, str]]:
+) -> List[dict[str, str]]:
     """Serialize only messages role and content.
-    
-    It uses a mapping to change the roles to supported values in outside models.
+
+    It uses a mapping to change the roles to supported values for other LLMs.
     If a role is not in the mapping, it will be ignored.
     """
     if "roles_mapping" not in kwargs:
         raise ValueError("'roles_mapping' dict must be provided.")
-    
+
     if not isinstance(messages, list):
         raise ValueError("The messages must be a list.")
     serialized_messages = []
@@ -44,9 +38,6 @@ def role_mapping_serializer(
         converted_role = kwargs["roles_mapping"].get(message.role, None)
         if converted_role:
             serialized_messages.append(
-                        {
-                            "content": message.content,
-                            "role": converted_role
-                        }
-                    )
+                {"content": message.content, "role": converted_role}
+            )
     return serialized_messages
